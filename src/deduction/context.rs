f70903e2c59a0798b09c8520d58883ca91c2b4f8
JP O'Brien <jpobrien925@gmail.com>
2024-07-12 12:32:34 -0700

    Changed judgements and contexts to reflect tree of trees structure

--- src/deduction/context.rs
+++ src/deduction/context.rs
@@ -2,23 +2,44 @@ use crate::{
     deduction::judgement::{ContextPtr, JudgementKind},
     terms::{
         variable::{FreeVariable, VariableData},
-        TermData,
+        TermData, Type,
     },
 };
 
 type ParentPtr = usize;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct ContextTreeNode {
-    free_variable: Option<FreeVariable>,
-    parent: Option<ParentPtr>,
+struct JudgementTree {
     judgements: Vec<JudgementKind>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct TheDomain {
-    context_tree: Vec<ContextTreeNode>,
+struct ContextTreeNode {
+    free_variable: Option<FreeVariable>,
+    parent: Option<ParentPtr>,
+    judgement_tree: JudgementTree,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct ContextTree {
+    nodes: Vec<ContextTreeNode>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct TermArena {
     term_data: Vec<TermData>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) struct TheDomain {
+    context_tree: ContextTree,
+    term_data: TermArena,
+}
+
+impl JudgementTree {
+    fn new() -> Self {
+        Self { judgements: vec![] }
+    }
 }
 
 impl ContextTreeNode {
@@ -26,7 +47,27 @@ impl ContextTreeNode {
         Self {
             free_variable: None,
             parent: None,
-            judgements: vec![JudgementKind::WellFormed],
+            judgement_tree: JudgementTree::new(),
+        }
+    }
+}
+
+impl ContextTree {
+    fn new() -> Self {
+        Self {
+            nodes: vec![ContextTreeNode::root()],
+        }
+    }
+}
+
+impl TermArena {
+    fn new() -> Self {
+        Self { term_data: vec![] }
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        Self {
+            term_data: Vec::with_capacity(capacity),
         }
     }
 }
@@ -34,8 +75,8 @@ impl ContextTreeNode {
 impl TheDomain {
     pub(crate) fn new() -> Self {
         Self {
-            context_tree: vec![ContextTreeNode::root()],
-            term_data: vec![],
+            context_tree: ContextTree::new(),
+            term_data: TermArena::new(),
         }
     }
 
@@ -43,7 +84,7 @@ impl TheDomain {
         todo!()
     }
 
-    pub(crate) fn push_variable(&mut self, variable_data: VariableData) -> FreeVariable {
+    pub(crate) fn push_variable(&mut self, name: String, typ: Type) -> FreeVariable {
         todo!()
     }
 }
--- src/deduction/judgement.rs
+++ src/deduction/judgement.rs
@@ -40,7 +40,7 @@ pub(crate) struct ContextPtr(usize);
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct Judgement {
     context_ptr: ContextPtr,
-    term_ptr: JudgementPtr,
+    judgement_kind: JudgementKind,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -58,12 +58,23 @@ pub enum JError {
 }
 
 impl ContextPtr {
-    fn new() -> Self {
+    /// Creates a ContextPtr that points to the root of the ContextTree i.e. at index 0.
+    fn empty_context() -> Self {
         Self(0)
     }
 }
 
+impl JudgementPtr {
+    fn well_formed() -> Self {
+        Self::WellFormed
+    }
+}
+
 impl JudgementKind {
+    fn well_formed() -> Self {
+        Self::WellFormed
+    }
+
     #[inline]
     fn replace_with_wellformed(&mut self) -> Self {
         self.replace(Self::WellFormed)
@@ -78,8 +89,8 @@ impl JudgementKind {
 impl Judgement {
     pub(crate) fn new() -> Self {
         Self {
-            kind: JudgementKind::WellFormed,
-            context_ptr: ContextPtr::new(),
+            judgement_kind: JudgementKind::well_formed(),
+            context_ptr: ContextPtr::empty_context(),
         }
     }
 }
@@ -106,82 +117,22 @@ impl Deduction {
     ///
     /// If the Judgement is any other variant, or the name is taken we return an error.
     pub fn variable_introduction(&mut self, name: String) -> JResult {
-        // This looks weird because we do not have ownership over the Judgement.
-        match &self.judgement {
-            JudgementKind::Type(_) if !self.domain.contains_name_at(&name, self.context_ptr) => {
-                let JudgementKind::Type(typ) = self.judgement.replace_with_wellformed() else {
-                    // SAFETY: We just checked that the variant was Type above.
-                    unsafe { unreachable_unchecked() }
-                };
-                let variable_data = VariableData::new(name, typ);
-                let free_variable = self.domain.push_variable(variable_data);
-                todo!()
-            }
-            _ => Err(JError::Illegal(
-                "Judgement must be a Type to introduce a Variable.",
-            )),
-        }
+        todo!("Variable Introduction")
     }
 
     /// Forms the Natural Type.
     ///
     /// This can be done in any WellFormed context.
     pub fn natural_formation(&mut self) -> JResult {
-        match self.judgement {
-            JudgementKind::WellFormed => {
-                self.judgement = NaturalType.into();
-                Ok(())
-            }
-            _ => Err(JError::Illegal(
-                "Judgement must be Well Formed to introduce a Type.",
-            )),
-        }
+        todo!("Natural Formation")
     }
 
     pub fn universe_formation(&mut self, level: UniverseLevel) -> JResult {
-        if let JudgementKind::WellFormed = self.judgement {
-            self.judgement = Universe::new(level).into();
-            Ok(())
-        } else {
-            Err(JError::Illegal(
-                "Judgement must be Well Formed to introduce a Universe.",
-            ))
-        }
+        todo!("Universe Formation")
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_variable_introduction() {
-        let mut deduction = Deduction::new();
-        let _ = deduction.universe_formation(0);
-        assert_eq!(deduction.variable_introduction("x".to_string()), Ok(()));
-        assert_eq!(
-            deduction.variable_introduction("x".to_string()),
-            Err(JError::NameTaken("x".to_string()))
-        );
-    }
-
-    #[test]
-    fn test_natural_formation() {
-        let mut deduction = Deduction::new();
-        assert_eq!(deduction.natural_formation(), Ok(()));
-        assert_eq!(
-            deduction.judgement,
-            JudgementKind::Type(Type::NaturalType(NaturalType))
-        );
-    }
-
-    #[test]
-    fn test_universe_formation() {
-        let mut deduction = Deduction::new();
-        assert_eq!(deduction.universe_formation(0), Ok(()));
-        assert_eq!(
-            deduction.judgement,
-            JudgementKind::Type(Type::Universe(Universe::new(0)))
-        );
-    }
 }
