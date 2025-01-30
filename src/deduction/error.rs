9dd593ab3a64c3643ca05d8eaac8a098d832489c
j-p-obrien <96845005+j-p-obrien@users.noreply.github.com>
2025-01-29 18:39:30 -0800

    Delete src/deduction directory

--- src/deduction/error.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum JError {
-    Illegal(&'static str),
-    NameTaken(String),
-}
--- src/deduction/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod context_tree;
-pub mod error;
-pub mod judgement;
-pub mod term_arena;
-pub mod terminal;
-pub mod the_domain;
