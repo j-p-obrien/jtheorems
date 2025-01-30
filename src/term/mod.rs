fcd77d206d2a75b054426f6356917d6fbe8ea021
j-p-obrien <96845005+j-p-obrien@users.noreply.github.com>
2025-01-29 18:39:20 -0800

    Delete src/term directory

--- src/term/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod application;
-pub mod defined;
-pub mod lambda;
-pub mod primitives;
-pub mod terms;
-pub mod types;
-pub mod variable;
-
-pub use terms::*;
--- src/term/primitives/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod coproduct;
-pub mod empty;
-pub mod identity;
-pub mod naturals;
-pub mod pair;
-pub mod unit;
-pub mod universe;
